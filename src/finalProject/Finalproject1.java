2cf15a7f6d0aebcdbf70f73d5baf7919331e0c9a
andres1770 <andresmoron1770@gmail.com>
2022-09-28 20:48:15 -0400

    2nd commit

---
+++ src/finalProject/Finalproject1.java
@@ -0,0 +1,10 @@
+package finalProject;
+
+public class Finalproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the code for audi");
+	}
+
+}
--- src/finalProject/finalclass.java
+++ src/finalProject/finalclass.java
@@ -4,6 +4,7 @@ public class finalclass {
 
 	public static void main(String[] args) {
 		System.out.println(" this is the code for the mercedes");
+		
 	}
 
 }
